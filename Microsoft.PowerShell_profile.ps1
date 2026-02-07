29898af985d674c58932a0184d941560109629b5
Federico Rapetti <fuji1097@gmail.com>
2026-02-07 18:15:42 +0100

    Update profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,12 @@
 # Chocolatey profile
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
- Import-Module "$ChocolateyProfile"
+if (Test-Path($ChocolateyProfile))
+{
+  Import-Module "$ChocolateyProfile"
 }
+
+# Forcing UTF-8
+[Console]::OutputEncoding = [Text.Encoding]::UTF8
 
 # Oh My POSH Settings
 oh-my-posh init pwsh --config $env:POSH_THEMES_PATH/blue-owl.omp.json | Invoke-Expression
@@ -18,26 +22,54 @@ Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
 Set-PSReadLineOption -ShowToolTips
 
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
+Set-PsFzfOption -TabExpansion
+
+# Your PowerShell $PROFILE
+$env:VIRTUAL_ENV_DISABLE_PROMPT=1
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
 # su
-function Open-Admin {
-    $currPath = Get-Location
-    Start-Process -Verb RunAs cmd.exe "/c start wt.exe -d $currPath"
+function Open-Admin
+{
+  $currPath = Get-Location
+  Start-Process -Verb RunAs cmd.exe "/c start wt.exe -d $currPath"
 }
 
-function Get-GgpKeys {
-    param(
-        [string]$ExportParam
-    )
+function Get-GgpKeys
+{
+  param(
+    [string]$ExportParam
+  )
 
-    if ($ExportParam) {
-        gpg --armor --export $ExportParam
-    } else {
-        gpg --list-secret-keys --keyid-format=long 
-    }
+  if ($ExportParam)
+  {
+    gpg --armor --export $ExportParam
+  } else
+  {
+    gpg --list-secret-keys --keyid-format=long
+  }
 }
 
-function Clip-WorkingDirectory {
-    $pwd | Select-Object -ExpandProperty Path | Set-Clipboard
+function Clip-WorkingDirectory
+{
+  $pwd | Select-Object -ExpandProperty Path | Set-Clipboard
+}
+
+function Get-WhichDir
+{
+  param(
+    [string][Parameter(Mandatory = $true, Position = 0)]
+    $Command
+  )
+  which $Command | Select-Object -ExpandProperty Source
+}
+
+function Open-TerminalWorkingDirectory
+{
+  wt -d .
 }
 
 Set-Alias su Open-Admin
@@ -45,3 +77,6 @@ Set-Alias gpg-keys Get-GPGKeys
 Set-Alias cds Set-SpecialLocation
 Set-Alias which Get-Command
 Set-Alias ccd Clip-WorkingDirectory
+Set-Alias wtd Open-TerminalWorkingDirectory
+
+Import-Module -Name Microsoft.WinGet.CommandNotFound
